feae87b4f11a2f280fa9f81e33e48effa8302d46
Dennis Schwertel <dennisschwertel@gmail.com>
2019-10-01 16:39:56 +0200

    fixed remove function

--- bin/remove.py
+++ bin/remove.py
@@ -13,15 +13,22 @@ def log(string):
     click.echo(f"fam-flavour: {string}")
 
 
-def remove_requirement(flavour_package_name):
+def remove_requirement(flavour_package_name, version):
     with Path("app.flavour").open("r") as f:
         yml = f.read()
 
         yaml_data = load(yml, libflavour.schema.schema_project)
-
-        if flavour_package_name in yaml_data["addons"]:
+        name_and_version = f"{flavour_package_name}:{version}"
+        if name_and_version in yaml_data["addons"]:
             log("deleting")
-            del yaml_data["addons"][flavour_package_name]
+            p = (
+                Path(".flavour")
+                / "addons"
+                / str(yaml_data["addons"][name_and_version]["hash"])
+            )
+            p.unlink()
+            del yaml_data["addons"][name_and_version]
+
             log("can not add to app.flavour, addon entry already exists")
         else:
             log("could not find entry")
@@ -34,7 +41,7 @@ def remove_requirement(flavour_package_name):
 def add():
     yaml = click.get_text_stream("stdin").read()
     yaml_data = load(yaml, libflavour.schema.schema_addon)
-    remove_requirement(yaml_data["meta"]["name"])
+    remove_requirement(yaml_data["meta"]["name"], yaml_data["meta"]["version"])
 
 
 if __name__ == "__main__":
